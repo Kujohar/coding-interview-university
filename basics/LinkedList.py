35556e665f09ea94a038154d846864a80697242e
Kujohar <khalid.u.johar@gmail.com>
2023-02-11 20:56:40 -0500

    Pull Leetcode data using GraphQL

---
+++ basics/LinkedList.py
@@ -0,0 +1,12 @@
+# %%
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.data.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.data = Node
+
+# %%
--- leetcode/PullSubmissions.py
+++ leetcode/PullSubmissions.py
@@ -1,18 +1,27 @@
-import requests,json
+import requests
+import json
 
 
 url = "https://leetcode.com/graphql/"
-query = {
-    "query": "\n    query recentAcSubmissions($username: String!, $limit: Int!) {\n  recentAcSubmissionList(username: $username, limit: $limit) {\n    id\n    title\n    titleSlug\n    timestamp\n  }\n}\n    ",
-    "variables": {
-        "username": "kujohar999",
-        "limit":100
-        }
-        }
 
-response = requests.post(url,json = query)
+GraphQLQuery = """
+query recentAcSubmissions($username: String!, $limit: Int!) {
+	recentAcSubmissionList(username: $username, limit: $limit) {
+		id
+		title
+		titleSlug
+		timestamp
+	}
+}
+"""
+variables = {
+    "username": "kujohar999",
+    "limit": 100
+}
+
+response = requests.post(
+    url, json={'query': GraphQLQuery, 'variables': variables})
 print(response.json())
-
 
 # with open("sample.json", "w") as outfile:
 #     json.dump(response.json(), outfile)
