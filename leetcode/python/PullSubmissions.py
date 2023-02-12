219e61aa1a1ccc4b9bf23cd8dbb67e7b88bd6dce
Kujohar <khalid.u.johar@gmail.com>
2023-02-11 22:45:32 -0500

    update python file to use pull GraphQL data to json

--- leetcode/python/PullSubmissions.py
+++ leetcode/python/PullSubmissions.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 
 
 url = "https://leetcode.com/graphql/"
@@ -11,6 +12,11 @@ query recentAcSubmissions($username: String!, $limit: Int!) {
 		title
 		titleSlug
 		timestamp
+		statusDisplay
+		lang
+		url
+		runtime
+		memory
 	}
 }
 """
@@ -21,7 +27,12 @@ variables = {
 
 response = requests.post(
     url, json={'query': GraphQLQuery, 'variables': variables})
-print(response.json())
+# print(response.json())
+# print(response.cookies)
 
-# with open("sample.json", "w") as outfile:
-#     json.dump(response.json(), outfile)
+
+D = datetime.datetime.now()
+with open("../LeetCodeData" +
+          D.strftime("%Y-%m-%d") +
+          ".json", "w") as outfile:
+    json.dump(response.json(), outfile)
