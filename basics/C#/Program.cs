7e7970f48df19a6c625559f9395bd99c98fb9c84
Kujohar <khalid.u.johar@gmail.com>
2023-02-21 21:07:40 -0500

    seperated leetcode data into leetcodeData repo

--- basics/C#/Program.cs
+++ basics/C#/Program.cs
@@ -29,17 +29,3 @@ unsafe
 
 Console.WriteLine("--------");
 Console.WriteLine(a[0]);
-
-// using System;
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         int val = 50;
-//         // int* x = &val;
-
-//         Console.WriteLine("Data: {0} ", val);
-//         // Console.WriteLine("Address: {0}", (int)x);
-//         // Console.ReadKey();
-//     }
-// }
